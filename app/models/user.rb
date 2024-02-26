752c087b70b258cda1b86cc9eb15b6f637351559
mateuszbialowas <mateusz.bialowas2@gmail.com>
2024-02-27 00:02:33 +0100

    add roles to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  ADMIN = 'admin'
+  CLIENT = 'client'
+
+  ROLES = [ADMIN, CLIENT].freeze
+
+  enum role: { admin: ADMIN, client: CLIENT }
 end

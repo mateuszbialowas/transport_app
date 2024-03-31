6f1aa09be6b0161b3b482f5817ae8dd563cf3685
mateuszbialowas <mateusz.bialowas2@gmail.com>
2024-03-31 21:19:18 +0200

    rubocop

--- db/migrate/20240226225454_add_role_to_users.rb
+++ db/migrate/20240226225454_add_role_to_users.rb
@@ -2,6 +2,6 @@
 
 class AddRoleToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :role, :string, null: false
+    add_column :users, :role, :string, null: false # rubocop:disable Rails/NotNullColumn
   end
 end

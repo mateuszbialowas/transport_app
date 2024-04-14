a91f8d4e480679a898536b438547da1351f191f0
mateuszbialowas <mateusz.bialowas2@gmail.com>
2024-04-14 19:36:54 +0200

    scaffold documents

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,70 @@
+class DocumentsController < ApplicationController
+  before_action :set_document, only: %i[ show edit update destroy ]
+
+  # GET /documents or /documents.json
+  def index
+    @documents = Document.all
+  end
+
+  # GET /documents/1 or /documents/1.json
+  def show
+  end
+
+  # GET /documents/new
+  def new
+    @document = Document.new
+  end
+
+  # GET /documents/1/edit
+  def edit
+  end
+
+  # POST /documents or /documents.json
+  def create
+    @document = Document.new(document_params)
+
+    respond_to do |format|
+      if @document.save
+        format.html { redirect_to document_url(@document), notice: "Document was successfully created." }
+        format.json { render :show, status: :created, location: @document }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /documents/1 or /documents/1.json
+  def update
+    respond_to do |format|
+      if @document.update(document_params)
+        format.html { redirect_to document_url(@document), notice: "Document was successfully updated." }
+        format.json { render :show, status: :ok, location: @document }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /documents/1 or /documents/1.json
+  def destroy
+    @document.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to documents_url, notice: "Document was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_document
+      @document = Document.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def document_params
+      params.require(:document).permit(:cmr_number, :sender_1, :consignee_2, :delivery_place_3, :taking_over_place_4, :taking_over_date_4, :documents_5, :marks_6_1, :number_7_1, :method_8_1, :nature_9_1, :number_10_1, :weight_11_1, :volume_12_1, :marks_6_2, :number_7_2, :method_8_2, :nature_9_2, :number_10_2, :weight_11_2, :volume_12_2, :marks_6_3, :number_7_3, :method_8_3, :nature_9_3, :number_10_3, :weight_11_3, :volume_12_3, :marks_6_4, :number_7_4, :method_8_4, :nature_9_4, :number_10_4, :weight_11_4, :volume_12_4, :class_6, :number_7, :letter_8, :adr_9, :sender_instructions_13, :carriage_instructions_14, :carriage_paid_14, :carriage_forward_14, :cash_on_delivery_15, :carrier_16, :carriers_plates_16, :successive_carriers_17, :carriers_reservations_18, :special_agreements_19, :established_in_21, :established_in_date_21)
+    end
+end
---
+++ db/migrate/20240414173345_create_documents.rb
@@ -0,0 +1,59 @@
+class CreateDocuments < ActiveRecord::Migration[7.1]
+  def change
+    create_table :documents do |t|
+      t.string :cmr_number
+      t.text :sender_1
+      t.text :consignee_2
+      t.text :delivery_place_3
+      t.text :taking_over_place_4
+      t.date :taking_over_date_4
+      t.text :documents_5
+      t.string :marks_6_1
+      t.string :number_7_1
+      t.string :method_8_1
+      t.string :nature_9_1
+      t.string :number_10_1
+      t.string :weight_11_1
+      t.string :volume_12_1
+      t.string :marks_6_2
+      t.string :number_7_2
+      t.string :method_8_2
+      t.string :nature_9_2
+      t.string :number_10_2
+      t.string :weight_11_2
+      t.string :volume_12_2
+      t.string :marks_6_3
+      t.string :number_7_3
+      t.string :method_8_3
+      t.string :nature_9_3
+      t.string :number_10_3
+      t.string :weight_11_3
+      t.string :volume_12_3
+      t.string :marks_6_4
+      t.string :number_7_4
+      t.string :method_8_4
+      t.string :nature_9_4
+      t.string :number_10_4
+      t.string :weight_11_4
+      t.string :volume_12_4
+      t.string :class_6
+      t.string :number_7
+      t.string :letter_8
+      t.string :adr_9
+      t.text :sender_instructions_13
+      t.string :carriage_instructions_14
+      t.string :carriage_paid_14
+      t.string :carriage_forward_14
+      t.string :cash_on_delivery_15
+      t.text :carrier_16
+      t.text :carriers_plates_16
+      t.text :successive_carriers_17
+      t.text :carriers_reservations_18
+      t.text :special_agreements_19
+      t.string :established_in_21
+      t.string :established_in_date_21
+
+      t.timestamps
+    end
+  end
+end

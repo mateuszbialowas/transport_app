2b902ab4b41fb975bdc40dc6dddb8d05d767c8a0
mateuszbialowas <mateusz.bialowas2@gmail.com>
2024-05-26 19:56:46 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,28 +4,19 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/emails' if Rails.env.development?
 
   devise_for :users, controllers: { invitations: 'users/invitations' }
-  devise_scope :user do
-    authenticated :user do
-      root 'static_pages#home'
-      resources :documents do
-        collection do
-          resources :available_hours, only: [:index], module: :documents
-        end
-      end
-
-      resources :calendar, only: [:index] do
-        collection do
-          get :events
-        end
-      end
-    end
-
-    resources :users, only: [:index]
-
-    unauthenticated do
-      root 'devise/sessions#new', as: :unroot
+  root 'static_pages#home'
+  resources :documents do
+    collection do
+      resources :available_hours, only: [:index], module: :documents
     end
   end
+
+  resources :calendar, only: [:index] do
+    collection do
+      get :events
+    end
+  end
+  resources :users, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

f559b74206f3d0ace1742e2ebef60592a5389e66
mateuszbialowas <mateusz.bialowas2@gmail.com>
2024-06-01 23:29:08 +0200

    add mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: Rails.application.credentials.mailgun,
+    domain: 'mg.argoksh.eu',
+    api_host: 'api.eu.mailgun.net'
+    # timeout: 20 # Default depends on rest-client, whose default is 60s. Added in 1.2.3.
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
